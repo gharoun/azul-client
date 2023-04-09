74630d8f6b5a3b1b750b7419051060a55c9061de
Gaya Haroun <gharoun94@gmail.com>
2023-04-09 14:27:07 -0500

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import Customers from "./components/Customers";
 import Movies from "./components/Movies";
-
+import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Rentals from "./components/Rentals";
+import NoPage from "./components/NoPage";
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <NavBar />
+      <Routes>
+        <Route path="/Movies" element={<Movies />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/rentals" element={<Rentals />} />
+        <Route path="/" element={<Movies />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </main>
   );
 }
